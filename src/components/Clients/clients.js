07b1dd175a4c6bc9d1e8d92b6b68e9007009afdf
naren <blazeraze.naren@gmail.com>
2019-11-30 00:40:48 +0200

    client component updated to use real images; updated packages

--- src/components/Clients/clients.js
+++ src/components/Clients/clients.js
@@ -1,120 +1,82 @@
-import React, { Component } from "react"
+import React, { useState } from "react"
+import Img from "gatsby-image"
 import {
   Carousel,
   CarouselItem,
   CarouselControl,
   CarouselIndicators,
-  CarouselCaption,
 } from "reactstrap"
 import clientMod from "./clients.module.css"
 
-const items = [
-  {
-    src:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EClient%20One%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-    // altText: "Client 1",
-    // caption: "Client 1",
-  },
-  {
-    src:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3EClient%20Two%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-    // altText: "Client 2",
-    // caption: "Client 2",
-  },
-  {
-    src:
-      "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EClient%20Three%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
-    // altText: "CLient 3",
-    // caption: "Client 3",
-  },
-]
+const Clients = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(0)
+  const [animating, setAnimating] = useState(false)
 
-class Clients extends Component {
-  constructor() {
-    super()
-    this.state = {
-      activeIndex: 0,
-    }
-  }
-
-  previous = () => {
-    const { activeIndex } = this.state
+  const previous = () => {
+    if (animating) return
     const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1
-    this.setState({ activeIndex: nextIndex })
+    setActiveIndex(nextIndex)
   }
 
-  next = () => {
-    const { activeIndex } = this.state
+  const next = () => {
+    if (animating) return
     const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1
-    this.setState({
-      activeIndex: nextIndex,
-    })
+    setActiveIndex(nextIndex)
   }
 
-  goToIndex = newIndex => {
-    this.setState({ activeIndex: newIndex })
+  const goToIndex = newIndex => {
+    if (animating) return
+    setActiveIndex(newIndex)
   }
 
-  render() {
-    const { activeIndex } = this.state
-    const slides = items.map(item => {
-      return (
-        <CarouselItem
-          onExiting={this.onExiting}
-          onExited={this.onExited}
-          key={item.src}
-          style={{ position: "relative" }}
-          className="mb-5"
-        >
-          <div className={clientMod.clientImgCont}>
-            <img
-              className={clientMod.clientImg}
-              src={item.src}
-              alt={item.altText}
-            />
-          </div>
-          <div className={clientMod.clientProfile}>
-            <h1>Coming Soon....</h1>
-            <p>You will get to hear from our happy clients very soon.</p>
-          </div>
-          {/* <CarouselCaption
-            captionText={item.caption}
-            captionHeader={item.caption}
-          /> */}
-        </CarouselItem>
-      )
-    })
+  const slides = items.map(item => {
     return (
-      <div className={clientMod.clients}>
-        <h1 className="text-center py-5">Our happy clients</h1>
-        <div className={clientMod.carousel}>
-          <Carousel
-            activeIndex={activeIndex}
-            next={this.next}
-            previous={this.previous}
-          >
-            <CarouselIndicators
-              className="mt-5"
-              items={items}
-              activeIndex={activeIndex}
-              onClickHandler={this.goToIndex}
-            />
-            {slides}
-            <CarouselControl
-              direction="prev"
-              directionText="Previous"
-              onClickHandler={this.previous}
-            />
-            <CarouselControl
-              direction="next"
-              directionText="Next"
-              onClickHandler={this.next}
-            />
-          </Carousel>
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src.src}
+        style={{ position: "relative" }}
+        className="mb-5"
+      >
+        <Img
+          className={clientMod.clientImgCont}
+          fluid={item.src}
+          alt={item.altText}
+        />
+        <div className={clientMod.clientProfile}>
+          <h1>{item.name}</h1>
+          <h4>{item.address}</h4>
+          <p>{item.saying}</p>
         </div>
-      </div>
+      </CarouselItem>
     )
-  }
+  })
+  return (
+    <div className={clientMod.clients}>
+      <h1 className="text-center py-5">Our happy clients</h1>
+      <div className={clientMod.carousel}>
+        <Carousel activeIndex={activeIndex} next={next} previous={previous}>
+          <CarouselIndicators
+            className="mt-5"
+            items={items}
+            activeIndex={activeIndex}
+            onClickHandler={goToIndex}
+          />
+          {slides}
+          <CarouselControl
+            direction="prev"
+            directionText="Previous"
+            onClickHandler={previous}
+          />
+          <CarouselControl
+            direction="next"
+            directionText="Next"
+            onClickHandler={next}
+          />
+        </Carousel>
+      </div>
+    </div>
+  )
 }
 
 export default Clients
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,75 +8,109 @@ import "../components/index.css"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" keywords={[`cable network`, `internet`, `budhabare`]} />
-    <div style={{ margin: "1em 0" }}>
-      <h1 className="text-center">Services</h1>
-      <h4 className="text-center mb-5" style={{ letterSpacing: -1 }}>
-        Get the <strong style={{ textTransform: "uppercase" }}>best</strong>{" "}
-        package at reasonable price.
-      </h4>
-      <div className="container">
-        <div className="row pl-md-5">
-          <div className="col-sm-6 col-md-4">
-            <div className="card text-white mb-4">
-              <Img
-                fluid={data.image1.childImageSharp.fluid}
-                className="card-img"
-              />
-              <div
-                className="card-img-overlay"
-                style={{ background: "rgba(0,0,0, 0.6)" }}
-              >
-                <h5 className="text-center">Cable TV</h5>
-                <Link className="btn btn-view" to="/services">
-                  View
-                </Link>
+const IndexPage = ({ data }) => {
+  // const clients = [
+  //   data.gyanu.childImageSharp.fluid,
+  //   data.sailesh.childImageSharp.fluid,
+  //   data.anns.childImageSharp.fluid,
+  // ]
+  const items = [
+    {
+      src: data.gyanu.childImageSharp.fluid,
+      altText: "client gyanu",
+      name: "Gyanu Thapa",
+      address: "Burney, Budhabare 4",
+      saying:
+        "I am using wireless net provided by Budhabare Space Cable since 2015 and cable TV since 2010. I recommend to use this network to you all.",
+    },
+    {
+      src: data.sailesh.childImageSharp.fluid,
+      altText: "client sailesh",
+      name: "Sailesh Poudel",
+      address: "Gadigaun, Buddhashanti 6",
+      saying:
+        "I live in rural part of Buddhashanti and this network has provided me service in a well facilitated way.",
+    },
+    {
+      src: data.anns.childImageSharp.fluid,
+      altText: "client anns",
+      name: "Anns Basnet",
+      address: "Burney, Budhabare 4",
+      saying:
+        "I am happy customer of Budhabare Space Cable, which provides internet and Cable TV with well managed service and customer care.",
+    },
+  ]
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`cable network`, `internet`, `budhabare`]} />
+      <div style={{ margin: "1em 0" }}>
+        <h1 className="text-center">Services</h1>
+        <h4 className="text-center mb-5" style={{ letterSpacing: -1 }}>
+          Get the <strong style={{ textTransform: "uppercase" }}>best</strong>{" "}
+          package at reasonable price.
+        </h4>
+        <div className="container">
+          <div className="row pl-md-5">
+            <div className="col-sm-6 col-md-4">
+              <div className="card text-white mb-4">
+                <Img
+                  fluid={data.image1.childImageSharp.fluid}
+                  className="card-img"
+                />
+                <div
+                  className="card-img-overlay"
+                  style={{ background: "rgba(0,0,0, 0.6)" }}
+                >
+                  <h5 className="text-center">Cable TV</h5>
+                  <Link className="btn btn-view" to="/services">
+                    View
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6 col-md-4">
-            <div className="card text-white mb-4">
-              <Img
-                fluid={data.image2.childImageSharp.fluid}
-                className="card-img"
-              />
-              <div
-                className="card-img-overlay"
-                style={{ background: "rgba(0,0,0, 0.6)" }}
-              >
-                <h5 className="text-center">Fiber Internet</h5>
-                <Link className="btn btn-view" to="/services">
-                  View
-                </Link>
+            <div className="col-sm-6 col-md-4">
+              <div className="card text-white mb-4">
+                <Img
+                  fluid={data.image2.childImageSharp.fluid}
+                  className="card-img"
+                />
+                <div
+                  className="card-img-overlay"
+                  style={{ background: "rgba(0,0,0, 0.6)" }}
+                >
+                  <h5 className="text-center">Fiber Internet</h5>
+                  <Link className="btn btn-view" to="/services">
+                    View
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6 col-md-4">
-            <div className="card text-white mb-4">
-              <Img
-                fluid={data.image3.childImageSharp.fluid}
-                className="card-img"
-              />
-              <div
-                className="card-img-overlay"
-                style={{ background: "rgba(0,0,0, 0.6)" }}
-              >
-                <h5 className="text-center">Wireless</h5>
-                <Link className="btn btn-view" to="/services">
-                  View
-                </Link>
+            <div className="col-sm-6 col-md-4">
+              <div className="card text-white mb-4">
+                <Img
+                  fluid={data.image3.childImageSharp.fluid}
+                  className="card-img"
+                />
+                <div
+                  className="card-img-overlay"
+                  style={{ background: "rgba(0,0,0, 0.6)" }}
+                >
+                  <h5 className="text-center">Wireless</h5>
+                  <Link className="btn btn-view" to="/services">
+                    View
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>{" "}
-    <Clients />
-    <Contact />
-  </Layout>
-)
+      </div>{" "}
+      <Clients items={items} />
+      <Contact />
+    </Layout>
+  )
+}
 
 export default IndexPage
 
@@ -84,6 +118,13 @@ export const serviceImage = graphql`
   fragment serviceImage on File {
     childImageSharp {
       fluid(maxWidth: 300, maxHeight: 200) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+  fragment clientImage on File {
+    childImageSharp {
+      fluid(maxWidth: 350, maxHeight: 200) {
         ...GatsbyImageSharpFluid
       }
     }
@@ -105,15 +146,15 @@ export const query = graphql`
     }
 
     gyanu: file(relativePath: { eq: "gyanu.jpg" }) {
-      ...serviceImage
+      ...clientImage
     }
 
     sailesh: file(relativePath: { eq: "sailesh.jpeg" }) {
-      ...serviceImage
+      ...clientImage
     }
 
     anns: file(relativePath: { eq: "anns.jpg" }) {
-      ...serviceImage
+      ...clientImage
     }
   }
 `
